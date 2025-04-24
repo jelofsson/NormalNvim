8e2ba5ca26c1d771733c651648109bd3228d0829
Jimmi Elofsson <jimmi.elofsson@inter.ikea.com>
2025-04-24 16:05:00 +0200

    Disable inline diagnostic text by default

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -902,12 +902,7 @@ return {
       -- get neotest namespace (api call creates or returns namespace)
       local neotest_ns = vim.api.nvim_create_namespace "neotest"
       vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
+        virtual_text = false, -- Disable inline diagnostic text
       }, neotest_ns)
       require("neotest").setup(opts)
     end,
